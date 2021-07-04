60a725f78afc43c5d22b71619eaf5421187e607d
AbhayM <75209124+AbhayMudgal@users.noreply.github.com>
2021-07-04 15:24:42 +0530

    Add files via upload

--- stacks/Min_knight_moves.cpp
+++ stacks/Min_knight_moves.cpp
@@ -5,13 +5,15 @@ int x[8] = {1, 2, -1, 2, 1, -2, -2, -1};
 int y[8] = {2, 1, 2, -1, -2, 1, -1, -2};
 
 bool check(int n, int m, int c, int d){
+  //function to check if the current jump is within the limits of the board
   if (c<0||c>=n) return false;
   if (d<0||d>=m) return false;
   return true;
 }
 
 int capture(int n, int m, int a, int b, int c, int d){
-
+  /* A stack of plausible moves is created and then checked in order if further moves
+  generate the desired outcome.*/
   stack<vector<int>> s;
   s.push({c-1, d-1, 0});
   while (!s.empty()){
